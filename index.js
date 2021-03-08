e3c5142313011d27480b5e9dcd4624255004ad29
Carl Brand <carl.brand@bit-g.co.uk>
2021-03-08 20:07:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const fetch = require('isomorphic-unfetch');
+
+class Flexpool {
+  constructor() {
+    this.basePath = 'https://flexpool.io/api/v1';
+  }
+
+  request(endpoint = '', options = {}) {
+    const url = this.basePath + endpoint;
+
+    const headers = {
+      'Content-type': 'application/json',
+    };
+
+    const config = {
+      ...options,
+      ...headers,
+    };
+
+    return fetch(url, config).then((result) => {
+      if (result.ok) {
+        return result.json();
+      }
+
+      throw new Error(result);
+    });
+  }
+
+  getPoolHashrate() {
+    const url = '/pool/hashrate';
+    return this.request(url, {});
+  }
+}
+
+module.exports = Flexpool;
---
+++ test.js
@@ -0,0 +1,11 @@
+const Flexpool = require('.');
+
+test('should be defined', () => {
+  expect(Flexpool).toBeDefined();
+});
+
+test('should return new Flexpool client', () => {
+  const client = new Flexpool();
+  expect(client.getPoolHashrate).toBeDefined();
+  expect(client.getMinersOnline).toBeDefined();
+});
